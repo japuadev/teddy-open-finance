9b5629fa092af098fd2a7caf85b3dec271b77eaa
japuadev <dimeloarquitetura@gmail.com>
2025-06-03 01:52:23 -0300

    feat:Initial commit

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app/app.module';
+import 'dotenv/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Teddy Open Finance - URL SHORTENER API')
+    .setDescription('API built to be URL shorteners for authenticated and unauthenticated users.')
+    .setVersion('0.1.0')
+    .addTag('documentation')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+bootstrap();
