cb93c734113c88b112103ad81db88dc77017e03a
japuadev <dimeloarquitetura@gmail.com>
2025-06-07 21:48:33 -0300

    feat(pipe): created custom pipe to uuid fields

--- src/pipes/custom-uuid.pipe.ts
+++ src/pipes/custom-uuid.pipe.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
 import { validate as isUUID } from 'uuid';
 
